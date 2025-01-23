0e6d759416766ad85a181a2d53cf3ee7c6f87e33
Chelsea <gzd2yk@Virginia.edu>
2025-01-23 12:17:53 -0500

    updated hw1

--- assignment/hw1.py
+++ assignment/hw1.py
@@ -1,12 +1,3 @@
-# Question 1.3: I was surprised flight attendants make so much despite only requiring a high school diploma.
-# Question 1.4: Software Developer, Data Scientist, Financial Analyst, Accountant, Flight Attendant, 
-                # Advertising/Promotions/Marketing Managers, Lawyer, Physicians/Surgeons
-                # I ranked the first 4 jobs based on all needing a bachelors degree and by most pay. 
-                # I ranked flight attendant next because it pays a less but has a low entry requirement. 
-                # I ranked the manager positions next because it requires previous work experience. 
-                # I ranked lawyer next because it requires a professional degrees. 
-                # I ranked physician last because it requires a professional degrees and more job training.
-
 # Question 2
 import numpy as np
 import matplotlib.pyplot as plt
