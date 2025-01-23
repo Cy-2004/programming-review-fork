c9de1e9553b2d806f7b7c928e7219c43c666b5ce
Chelsea <gzd2yk@Virginia.edu>
2025-01-23 01:45:33 -0500

    finished question 2

--- assignment/hw1.py
+++ assignment/hw1.py
@@ -7,4 +7,30 @@
                 # I ranked lawyer next because it requires a professional degrees. 
                 # I ranked physician last because it requires a professional degrees and more job training.
 
-# Question 
+# Question 2
+import numpy as np
+import matplotlib.pyplot as plt
+
+# part 1
+x = np.linspace(0,1,50)
+y = np.log(x)
+z = np.exp(x)
+plt.scatter(x,y, label='Natural Log')
+plt.scatter(x,z, label='Exponential')
+plt.xlabel("x-axis")
+plt.ylabel("y-axis")
+plt.title("Natural Log and Exponential Functions")
+plt.legend(loc = 'lower right')
+plt.show()
+
+# part 2
+grid = np.linspace(-6.5,6.5,100)
+sin = np.sin(grid)
+cos = np.cos(grid)
+plt.plot(grid,sin, label="Sine")
+plt.plot(grid,cos, label="Cosine")
+plt.xlabel("x-axis")
+plt.ylabel("y-axis")
+plt.title("Sine and Cosine Functions")
+plt.legend(loc = 'lower left')
+plt.show()
